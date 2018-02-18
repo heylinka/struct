ef2738482ab7d9b4d3609a086c9a3e4e4e34b331
heylinka <35678588+heylinka@users.noreply.github.com>
2018-02-19 01:16:56 +0500

    Add files via upload

---
+++ elinasoadlab1/elinasoadlab1/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+
+namespace elinasoadlab1
+{
+
+    struct People {
+        public int Id;
+        public string Name;
+        public string Meaning;
+        public string Gender;
+        public string Origin;
+        public int PeopleCount;
+
+    }
+
+
+
+    class Program
+    {
+
+        //Сортировка пузырьком
+        static void BubbleSort(List<People> people)
+        {
+            People temp;
+            for (int i = 0; i < people.Count; i++)
+            {
+                for (int j = i + 1; j < people.Count; j++)
+                {
+                    if (people[i].PeopleCount > people[j].PeopleCount)
+                    {
+                        temp = people[i];
+                        people[i] = people[j];
+                        people[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        //Гномья сортировка 
+        static void gnomeSort(List<People> people)
+        {
+            int i = 1;
+            while (i < people.Count)
+            {
+                if (i == 0 || people[i - 1].PeopleCount <= people[i].PeopleCount)
+                    i++;
+                else {
+                    People temp = people[i];
+                    people[i] = people[i - 1];
+                    people[i - 1] = temp;
+                    i--;
+                }
+            }
+        }
+
+        //Сортировка Шелла
+        static void shellSort(List<People> people)
+        {
+            int step = people.Count / 2;
+            while (step > 0)
+            {
+                int i, j;
+                for (i = step; i < people.Count; i++)
+                {
+                    People value = people[i];
+                    for (j = i - step; (j >= 0) && (people[j].PeopleCount > value.PeopleCount); j -= step)
+                        people[j + step] = people[j];
+                    people[j + step] = value;
+                }
+                step /= 2;
+            }
+        }
+
+
+        //Пирамидальная сортировка 
+        static int add2pyramid(List<People> people, int i, int N)
+        {
+            int imax;
+            People buf;
+            if ((2 * i + 2) < N)
+            {
+                if (people[2 * i + 1].PeopleCount < people[2 * i + 2].PeopleCount) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N) return i;
+            if (people[i].PeopleCount < people[imax].PeopleCount)
+            {
+                buf = people[i];
+                people[i] = people[imax];
+                people[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+
+        static void Pyramid_Sort(List<People> people)
+        {
+            //step 1: building the pyramid
+            for (int i = people.Count / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = add2pyramid(people, i, people.Count);
+                if (prev_i != i) ++i;
+            }
+
+            //step 2: sorting
+            People buf;
+            for (int k = people.Count - 1; k > 0; --k)
+            {
+                buf = people[0];
+                people[0] = people[k];
+                people[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = add2pyramid(people, i, k);
+                }
+            }
+        }
+
+
+
+
+
+
+        //Считать файл
+        static void ReadFile(List<People> list)
+        {
+            try
+            {
+                using (StreamReader reader = File.OpenText("D:\\elina\\elinasoadlab1\\foreign_names.csv"))
+                {
+                    string[] text = new string[6];
+                    while (!reader.EndOfStream)
+                    {
+                        People ind = new People();
+                        text = reader.ReadLine().Split(';');
+                        ind.Id = Convert.ToInt32(text[0]);
+                        ind.Name = Convert.ToString(text[1]);
+                        ind.Meaning = Convert.ToString(text[2]);
+                        ind.Gender = Convert.ToString(text[3]);
+                        ind.Origin = Convert.ToString(text[4]);
+                        ind.PeopleCount = Convert.ToInt32(text[5]);
+                        list.Add(ind);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            List<People> people = new List<People>();
+
+            //Измерятор времени
+            Stopwatch watch = new Stopwatch();
+
+
+            ReadFile(people);
+            Console.WriteLine("Сортировка пузырьком");
+            watch.Start();
+            BubbleSort(people);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            watch.Restart();
+            people.Clear();
+
+
+            ReadFile(people);
+            Console.WriteLine("\nГномья сортировка");
+            watch.Start();
+            gnomeSort(people);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            watch.Restart();
+            people.Clear();
+
+
+
+            ReadFile(people);
+            Console.WriteLine("\nСортировка Шелла");
+            watch.Start();
+            shellSort(people);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            watch.Restart();
+            people.Clear();
+
+
+
+            ReadFile(people);
+            Console.WriteLine("\nПирамидальная сортировка");
+            watch.Start();
+            Pyramid_Sort(people);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            watch.Restart();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ praktika1/praktika1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace praktika1
+{
+    class Program
+    {
+
+        //Сортировка с выборкой
+        public static void Selection(int[] numbers)
+        {
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+
+                int min = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                    if (numbers[j] < numbers[min]) min = j;
+
+                int dummy = numbers[i];
+                numbers[i] = numbers[min];
+                numbers[min] = dummy;
+            }
+
+        }
+
+
+
+        //Сортировка с подсчетом
+        public static void CountingSort(int[] numbers)
+        {
+
+            int[] c = new int[10000];
+
+            for (int k = 0; k < c.Length; k++)
+                c[numbers[k]] = c[numbers[k]] + 1;
+
+            int i = 0;
+            for (int j = 0; j < c.Length; j++)
+                while (c[j] != 0)
+                {
+                    numbers[i] = j;
+                    c[j]--;
+                    i++;
+                }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            int[] numbers1 = new int[10000];
+            int[] numbers2 = new int[10000];
+            Random rnd = new Random();
+            Stopwatch watch = new Stopwatch();
+
+            for (int i = 0; i < numbers1.Length; i++)
+            {
+                numbers1[i] = rnd.Next(10000);
+                numbers2[i] = numbers1[i];
+            }
+
+            Console.WriteLine("Сортировка с выборкой");
+            watch.Start();
+            Selection(numbers1);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            watch.Restart();
+
+            Console.WriteLine("Сортировка с подсчетом");
+            watch.Start();
+            CountingSort(numbers2);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+
+            /*for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine(numbers[i]);*/
+
+            Console.ReadKey();
+
+        }
+    }
+}
